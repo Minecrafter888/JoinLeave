3cbced9e0ded805fe454e4dd8ee1e9c87218eead
minecrafter888 <kyandro2004@gmail.com>
2023-03-10 12:40:27 +0100

    files

--- src/main/java/me/hephaestus2023/test/EGCustom.java
+++ src/main/java/me/hephaestus2023/test/EGCustom.java
@@ -5,7 +5,9 @@
 
 package me.hephaestus2023.test;
 
+import me.hephaestus2023.test.JoinLeaveListener.ClassListener;
 import me.hephaestus2023.test.JoinLeaveListener.joinleaveListener;
+import me.hephaestus2023.test.JoinLeaveListener.onDrop;
 import me.hephaestus2023.test.commands.Class;
 import me.hephaestus2023.test.commands.Reload;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,6 +19,8 @@ public final class EGCustom extends JavaPlugin {
         // Plugin startup logic
         getConfig().options().copyDefaults();
         saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new onDrop(), this);
+        getServer().getPluginManager().registerEvents(new ClassListener(), this);
         getServer().getPluginManager().registerEvents(new joinleaveListener(), this);
         getCommand("reload-Test").setExecutor(new Reload());
         getCommand("Class").setExecutor(new Class());
--- src/main/java/me/hephaestus2023/test/JoinLeaveListener/ClassListener.java
+++ src/main/java/me/hephaestus2023/test/JoinLeaveListener/ClassListener.java
@@ -5,36 +5,92 @@
 
 package me.hephaestus2023.test.JoinLeaveListener;
 
+import me.hephaestus2023.test.EGCustom;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.Effect;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
 
 
 public class ClassListener implements Listener {
+    Plugin plugin = EGCustom.getPlugin(EGCustom.class);
 
     @EventHandler
-    public void ClassClick (InventoryClickEvent e){
+    public void ClassClick(InventoryClickEvent e) {
 
-        if (e.getView().getTitle().equalsIgnoreCase(ChatColor.GOLD + "Classes")){
+        Player p = (Player) e.getWhoClicked();
 
-            if (e.getCurrentItem() == null){
+
+        if (e.getView().getTitle().equalsIgnoreCase(ChatColor.GOLD + "Classes")) {
+
+            if (e.getCurrentItem() == null) {
                 return;
             }
 
 
-            if (e.getCurrentItem().getType() == Material.DIAMOND_SWORD){
+            if (e.getCurrentItem().getType() == Material.DIAMOND_SWORD) {
+                    int am = plugin.getConfig().getInt("Classes.DpsBuffMulti");
 
+                    p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, am));
+                    p.sendMessage(ChatColor.RED + "Attack class selected");
+                    ItemStack sword = new ItemStack(Material.IRON_SWORD);
+                    ItemMeta sm = sword.getItemMeta();
+                    sm.setDisplayName("Dps sword");
+                    sword.setItemMeta(sm);
+                    p.getInventory().addItem(sword);
+                }
+
+            if (e.getCurrentItem().getType() == Material.GOLDEN_APPLE) {
+
+                int amp = plugin.getConfig().getInt("Classes.HealerBuffMulti");
+
+                p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, amp));
+                p.sendMessage(ChatColor.GOLD + "Healer class selected");
+                ItemStack bow = new ItemStack(Material.BOW);
+                bow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
+                ItemStack arrow = new ItemStack(Material.TIPPED_ARROW);
+                PotionMeta ta = (PotionMeta) arrow.getItemMeta();
+                ta.setBasePotionData(new PotionData(PotionType.INSTANT_HEAL));
+                arrow.setItemMeta(ta);
+                ItemMeta healer = bow.getItemMeta();
+                healer.setDisplayName("Healer bow");
+                bow.setItemMeta(healer);
+                p.getInventory().addItem(bow);
+                p.getInventory().addItem(arrow);
 
 
             }
+            if (e.getCurrentItem().getType() == Material.SHIELD) {
 
+                int pli = plugin.getConfig().getInt("Classes.TankBuffMulti");
 
+                p.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, Integer.MAX_VALUE, pli));
+                p.sendMessage(ChatColor.WHITE + "Tank classes selected");
+                ItemStack shield = new ItemStack(Material.SHIELD);
+                ItemMeta sm = shield.getItemMeta();
+                sm.setDisplayName("Tank shield");
+                shield.setItemMeta(sm);
+                p.getInventory().addItem(shield);
 
-
+            }
             e.setCancelled(true);
         }
 
+
+
     }
+
 }
